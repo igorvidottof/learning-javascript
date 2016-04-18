50d8a201d16ab172b0d50fb6e7175b3d742202c4
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-04-18 10:33:30 -0300

    add shift and unshift

--- freeCodeCamp/manipulating-arrays.js
+++ freeCodeCamp/manipulating-arrays.js
@@ -1,10 +1,20 @@
 'use strict';
 
-//Adding to the end
 var myArray = [['John', 23], ['cat', 2]];
+
+//Adding to the end
 myArray.push(['dog', 3]);
+console.log(myArray);
 
 //Removing from the end
 var removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+
+//Adding to the beginning
+myArray.unshift(["Paul", 35]);
+console.log(myArray);
+
+//Removing from the beginning 
+removedFromMyArray = myArray.shift();
 console.log(removedFromMyArray);
 
