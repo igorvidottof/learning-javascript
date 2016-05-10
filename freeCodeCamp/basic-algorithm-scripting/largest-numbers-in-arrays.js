8ca78298009d4772d1bf17fc4ce165731eff81be
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-05-10 11:31:00 -0300

    add a new solution

--- freeCodeCamp/basic-algorithm-scripting/largest-numbers-in-arrays.js
+++ freeCodeCamp/basic-algorithm-scripting/largest-numbers-in-arrays.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// First Solution
 function largestOfFour(arr) {
   var newArr = [];
   var largest = 0;
@@ -14,6 +15,21 @@ function largestOfFour(arr) {
   }
   return newArr;
 }
+
+// Second Solution
+function largestOfFour(arr) {
+  var largest = 0;
+  return arr.map(function(subArr) { // loop through the arrays
+    largest = subArr[0]; // assign the first element of the array to largest
+    subArr.forEach(function(val) { // loop through the numbers of each array
+      if (val > largest) { // verify if the number is greater than the largest
+        largest = val; 
+      }
+    });
+    return largest; // return the largest number to the new array created by the map function
+  });
+}
+
 
 // test
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
