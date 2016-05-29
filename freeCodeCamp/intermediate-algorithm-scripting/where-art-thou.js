45584278d2178d8816063f9518c806d0ccb45ea3
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-05-29 14:41:08 -0300

    fix solution

--- freeCodeCamp/intermediate-algorithm-scripting/where-art-thou.js
+++ freeCodeCamp/intermediate-algorithm-scripting/where-art-thou.js
@@ -1,28 +1,30 @@
-
 function whereAreYou(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
   var sourceKeys = Object.keys(source);
+  var cont = 0;
 
   arr = collection.filter(function(obj) {
+    cont = 0;
     for(var i = 0; i < sourceKeys.length; i++) {
-      if(obj.hasOwnProperty(sourceKeys[i])) {
-        for(var key in obj) {
-          if(obj[key] === source[key]) { 
-            return obj;
-          }
+      if(obj.hasOwnProperty(sourceKeys[i])) { 
+        if(obj[sourceKeys[i]] === source[sourceKeys[i]]) { 
+          cont++;
+        }
+        if(cont === sourceKeys.length) {
+          return obj;
         }
       }
     }
   });  
-
   
   // Only change code above this line
   return arr;
 }
 
-// console.log(whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
-// whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 });
+// tests
+console.log(whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+console.log(whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }));
 console.log(whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }));
 console.log(whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }));
